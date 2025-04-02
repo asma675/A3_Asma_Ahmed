4f09c66c583dd73e76007866646d3a1fbb151025
asma675 <158836479+asma675@users.noreply.github.com>
2025-04-02 19:52:08 -0400

    Update WebConfig.java

--- backend/src/main/java/com/Assignment/A3_Asma_Ahmed/config/WebConfig.java
+++ backend/src/main/java/com/Assignment/A3_Asma_Ahmed/config/WebConfig.java
@@ -10,9 +10,9 @@ public class WebConfig implements WebMvcConfigurer {
     
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**")   // Allow CORS on all endpoints
-                .allowedOrigins("file://","null")  // Allow requests from your frontend (adjust the URL if needed)
-                .allowedMethods("GET", "POST", "PUT", "DELETE")  // Allow specific HTTP methods
-                .allowedHeaders("*");  // Allow any headers
+        registry.addMapping("/**")   
+                .allowedOrigins("file://","null")  
+                .allowedMethods("GET", "POST", "PUT", "DELETE")  
+                .allowedHeaders("*");  
     }
 }
